497c1cbd9d923474a0bf69bc78adebdd08e6863f
Stephania31 <acunaantunez@gmail.com>
2023-03-02 01:18:01 +0000

    semaforo terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,20 +7,29 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const [active, setActive] = useState("")
+  const [purple, setPurple] = useState (false)
+  
   useEffect(()=> {console.log(active)},[active])
 
+ 
+  
   return (
     <div className="semaforo">
 
       <div>
         <div id="trafficTop"></div>
         <div id="container">
-          <div className={"red-light" + `${active==="red-light"? " red-light-active" : ""}`} onClick={(e)=>{setActive(e.target.className)}}></div>
-		  <br />
+          <div className={"red-light" + `${active==="red-light"? " red-light-active" : ""}` + `${purple ? " purple-light-active" : "" }`} onClick={(e)=>{setActive(e.target.className)}}></div>
+  
+      <br/>
           <div className={"yellow-light" + `${active==="yellow-light"? " yellow-light-active" : ""}`} onClick={(e)=>{setActive(e.target.className)}}></div>
 		  <br />
           <div className={"green-light" + `${active==="green-light"? " green-light-active" : ""}`} onClick={(e)=>{setActive(e.target.className)}}></div>
         </div>
+        <br />
+        <button type="button" onClick={(e) => setPurple(!purple)}>Click me</button>
+        
+      
       </div>
     </div>
   );
